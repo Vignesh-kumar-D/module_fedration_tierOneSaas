3d020f5ef5a116d6a173f1396dbdece584c3c51a
legendvi <dvigneshkumar3@gmail.com>
2023-10-21 11:26:41 +0530

    modified app in container

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <>
-      <h1>Welocome to our platform!!</h1>
+      <h1>Welocome to our TierOne SaaS!!</h1>
       <hr />
       <MarketingApp />
     </>
