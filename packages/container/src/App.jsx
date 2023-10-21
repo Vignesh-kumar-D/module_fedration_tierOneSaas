b367ce51ed4028bb5744fac4edf2bda607be0257
legendvi <dvigneshkumar3@gmail.com>
2023-10-21 20:05:49 +0530

    added header component

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Typography from "@material-ui/core/Typography";
+import Header from "./components/Header";
 const App = () => {
   return (
-    <>
-      <Typography align="center" variant="h1" component="h2">
+    <BrowserRouter>
+      <Header />
+      <Typography align="center" variant="h3" component="h2">
         Welocome to our TierOne SaaS!!
       </Typography>
-      <hr />
       <MarketingApp />
-    </>
+    </BrowserRouter>
   );
 };
 
