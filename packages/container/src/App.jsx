46f44812f0c26251f302f0660482a6304ebf6c01
legendvi <dvigneshkumar3@gmail.com>
2023-10-21 17:39:05 +0530

    edited app.js

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
-
+import Typography from "@material-ui/core/Typography";
 const App = () => {
   return (
-    <div>
-      <h1>Welocome to our TierOne SaaS!!</h1>
+    <>
+      <Typography align="center" variant="h1" component="h2">
+        Welocome to our TierOne SaaS!!
+      </Typography>
       <hr />
       <MarketingApp />
-    </div>
+    </>
   );
 };
 
